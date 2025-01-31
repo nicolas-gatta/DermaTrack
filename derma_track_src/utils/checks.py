e787329b1fcf7afc446673e5ae17e57b5000e170
LiocansG <nico6041@hotmail.be>
2025-01-31 15:53:04 +0100

    Deleting useless comment

--- derma_track_src/utils/checks.py
+++ derma_track_src/utils/checks.py
@@ -12,19 +12,18 @@ def group_and_super_user_checks(group_names, redirect_url='/'):
     Returns:
         function: The wrapped view function with group access control.
     """
+    
     if isinstance(group_names, str):
-        group_names = [group_names]  # Convert a single group name to a list
+        group_names = [group_names] 
 
     def inner(view_func):
         
         @wraps(view_func)
         def _wrapper(request, *args, **kwargs):
-
-            # Check if the user belongs to at least one of the specified groups
+            
             if not request.user.groups.filter(name__in=group_names).exists() and not request.user.is_superuser:
                 return redirect(redirect_url)
             
-            # Proceed with the view if the user is in the group
             return view_func(request, *args, **kwargs)
         
         return _wrapper
