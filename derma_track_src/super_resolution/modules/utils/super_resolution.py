fb37cd72e6c1a1b7dc6c7661a1ca4dfa21987db5
LiocansG <nico6041@hotmail.be>
2025-02-02 21:12:34 +0100

    Starting code for super resolution

---
+++ derma_track_src/super_resolution/modules/utils/super_resolution.py
@@ -0,0 +1,50 @@
+import os
+import cv2
+import numpy as np
+import requests
+import torch
+
+from super_resolution.modules.SRCNN.model import SRCNN
+from super_resolution.modules.SRGAN.generator_model import SRGANGenerator
+from super_resolution.modules.ESRGAN.generator_model import ESRGANGenerator
+
+class SuperResolution:
+    def __init__(self, model_path):
+        
+        if not os.path.exists(model_path):
+            raise FileNotFoundError(f"Model file not found: {model_path}")
+        
+        self.mode = None
+        self.model = self.load_model(model_path)
+        
+    
+    def load_model(self, model_path):
+
+        checkpoint  = torch.load(model_path, weights_only=True)
+        
+        architecture = checkpoint["architecture"]
+        
+        self.mode = checkpoint["mode"]
+        
+        match(architecture):
+            
+            case "SRCNN":
+                return SRCNN.load_state_dict(checkpoint['model_state_dict'])
+            
+            case "SRGAN":
+                pass
+            
+            case "SRGAN":
+                pass
+    
+    def fetch_image(self, image_url):
+        
+        return None
+    
+    def apply_super_resolution(self, image):
+
+        return None
+    
+    def save_image(self, image, output_dir="processed_images", filename="super_resolved.png"):
+        
+        return None
