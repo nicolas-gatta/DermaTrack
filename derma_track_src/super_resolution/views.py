a295e115e5193711e4fc30da8791ad256615de33
LiocansG <nico6041@hotmail.be>
2025-02-02 21:10:55 +0100

    Update views.py

--- derma_track_src/super_resolution/views.py
+++ derma_track_src/super_resolution/views.py
@@ -8,6 +8,7 @@ from django.template.loader import render_to_string
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
 
 
 from .modules.SRCNN import train as srcnn_train
@@ -20,12 +21,13 @@ from .modules.utils.image_converter import ImageColorConverter, ImageConverter
 from .modules.utils.dataloader import H5ImagesDataset
 
 from utils.checks import group_and_super_user_checks
-from utils.path_finder import PathFinder
 # Create your views here.
 
 @login_required
 @group_and_super_user_checks(group_names=[""], redirect_url="/")
 def training_model(request):
+    
+    model_name = request.POST['name']
     
     architecture = request.POST["architecture"]
     
@@ -43,18 +45,29 @@ def training_model(request):
     
     num_workers = int(request.POST["num-workers"])
     
-    output_dir = PathFinder.get_complet_path(f"super_resolution/modules/{architecture}/output/{request.POST['name']}_{learning_rate}_{batch_size}_{num_epochs}_x{scale}")
+    train_dataset = request.POST["train-dataset"]
     
-    train_file, valid_file, eval_file = [dataset_exist_or_create(dataset = dataset, mode = mode, scale = scale, category = category) 
-                                         for dataset, category in [(request.POST["train-dataset"], "training"), 
-                                                                   (request.POST["valid-dataset"], "validation"), 
-                                                                   (request.POST["eval-dataset"], "evaluation")] 
+    valid_dataset = request.POST["valid-dataset"]
+    
+    eval_dataset = request.POST["eval-dataset"]
+    
+    output_dir = os.path.join(settings.BASE_DIR, "super_resolution","models", f"{model_name}_{learning_rate}_{batch_size}_{num_epochs}_x{scale}.pth")
+    
+    train_file, valid_file, eval_file = [_dataset_exist_or_create(dataset = dataset, mode = mode, scale = scale, category = category) 
+                                         for dataset, category in [(train_dataset, "training"), 
+                                                                   (valid_dataset, "validation"), 
+                                                                   (eval_dataset, "evaluation")] 
                                          ]
+    
+    model_name = JsonManager.training_results_to_json(architecture = architecture, model_name = model_name, train_file = train_dataset, valid_file = valid_dataset, 
+                                         eval_file = eval_dataset, mode = mode, scale = scale, learning_rate = learning_rate, seed = seed, 
+                                         batch_size = batch_size, num_epochs = num_epochs, num_workers = num_workers)
     
     match(architecture):
         
         case "SRCNN":
             srcnn_train.train_model(
+                model_name = model_name,
                 train_file = train_file, 
                 valid_file = valid_file,
                 eval_file = eval_file, 
@@ -94,12 +107,12 @@ def training_model(request):
     return render(request, 'partial/model_form.html', {"form": None})
     
 
-def dataset_exist_or_create(dataset, mode, scale, category):
+def _dataset_exist_or_create(dataset, mode, scale, category):
     
-    output_path = PathFinder.get_complet_path(f"super_resolution/dataset/{category}/{dataset}_{mode}_x{scale}.hdf5")
+    output_path = os.path.join(settings.BASE_DIR, "super_resolution", "datasets", f"{category}/{dataset}_{mode}_x{scale}.hdf5")
     
     if not os.path.exists(output_path):
-        create_h5_image_file(input_path = PathFinder.get_complet_path(f"super_resolution/base_dataset/{category}/{dataset}"),
+        create_h5_image_file(input_path = os.path.join(settings.BASE_DIR, "super_resolution", "base_datasets", category, dataset),
                             scale = scale,
                             output_path = output_path,
                             mode = ImageColorConverter[mode])
@@ -128,33 +141,82 @@ def dataset_form(request):
     
 @login_required
 @group_and_super_user_checks(group_names=[""], redirect_url="/")
+def test_model_view(request):
+    if request.headers.get('HX-Request'):
+        #form = TrainingForm()
+        return render(request, 'partial/test_model.html', {"form": None})
+    
+@login_required
+@group_and_super_user_checks(group_names=[""], redirect_url="/")
 def create_dataset(request):
     
-    scale = int(request.POST["scale"])
-    
-    dataset = request.POST["dataset"]
-    
-    mode = request.POST["mode"]
-    
-    category = request.POST["category"]
-    
-    create_h5_image_file(input_path = PathFinder.get_complet_path(f"super_resolution/base_dataset/{category}/{dataset}"),
-                         scale = scale,
-                         output_path = PathFinder.get_complet_path(f"super_resolution/dataset/{category}/{dataset}_{mode}_x{scale}.hdf5"),
-                         mode = ImageColorConverter[mode])
+    _dataset_exist_or_create(dataset = request.POST["dataset"], 
+                             mode = request.POST["mode"], 
+                             scale = int(request.POST["scale"]), 
+                             category = request.POST["category"])
     
     return render(request, 'partial/dataset_form.html', {"form": None})
 
 @login_required
 @group_and_super_user_checks(group_names=[""], redirect_url="/")
 def get_datasets(request, category):
-    base_path = PathFinder.get_complet_path(os.path.join("super_resolution", "base_dataset", category))
+    base_path = os.path.join(settings.BASE_DIR, "super_resolution", "base_datasets", category)
     try:
         datasets = os.listdir(base_path)
     except FileNotFoundError:
         datasets = []
     
     return JsonResponse({'datasets': datasets})
+
+@login_required
+@group_and_super_user_checks(group_names=[""], redirect_url="/")
+def get_test_images(request):
+    
+    base_path = os.path.join(settings.MEDIA_ROOT, "test")
+    
+    try:
+        images = os.listdir(base_path)
+    except FileNotFoundError:
+        images = []
+    
+    return JsonResponse({'images': images})
+
+@login_required
+@group_and_super_user_checks(group_names=[""], redirect_url="/")
+def degrade_and_save_image(request, name, scale):
+    
+    base_path = os.path.join(settings.MEDIA_ROOT, "test", name)
+    
+    output_path = os.path.join(settings.MEDIA_ROOT, "output_test", "degraded_image.png")
+    
+    image = cv2.imread(base_path)
+    
+    degraded_image = cv2.resize(image, (image.shape[1] // scale, image.shape[0] // scale), interpolation = cv2.INTER_CUBIC)
+    
+    degraded_image = cv2.resize(degraded_image, (image.shape[1], image.shape[0]), interpolation=cv2.INTER_NEAREST)
+    
+    cv2.imwrite(output_path, degraded_image)
+
+    return JsonResponse({'url': os.path.join(settings.MEDIA_URL, "output_test", "degraded_image.png")})
+
+@login_required
+@group_and_super_user_checks(group_names=[""], redirect_url="/")
+def get_test_image(request, name):
+    
+    base_path = os.path.join(settings.MEDIA_URL, "test", name)
+    
+    return JsonResponse({'url': base_path})
+
+@login_required
+@group_and_super_user_checks(group_names=[""], redirect_url="/")
+def get_models(request):
+    base_path = os.path.join(settings.BASE_DIR, "super_resolution", "models")
+    try:
+        models = os.listdir(base_path)
+    except FileNotFoundError:
+        models = []
+    
+    return JsonResponse({'models': models})
 
 
 def test_2(request):
@@ -164,45 +226,14 @@ def test_2(request):
     return redirect("/")
 
 def test_3(request):
-    updated_fields = {
-        ModelField.COMPLETION_STATUS:  "10 %",
-        ModelField.TRAINING_LOSSES: [1, 0.8, 0.7, 0.6, 0.5, 0.5, 0.8, 0.7, 0.9, 0.4],
-        ModelField.VALIDATION_LOSSES: [2, 1, 0.9, 0.85, 0.75, 0.4, 0.8, 0.9, 0.4, 0.5]
-    }
-    
-    JsonManager.update_model_data("Hello", updated_fields=updated_fields)
     
     return redirect("/")
 
 def test_4(request):
-    with H5ImagesDataset(PathFinder.get_complet_path(f"super_resolution/dataset/evaluation/Set5_{ImageColorConverter.BGR2YCrCb.name}_x2.hdf5")) as loader:
-        print(f"Total images in dataset: {len(loader)}")
-
-        # Example: Load and display the first image pair
-        low_res, _ = loader.get_raw_data(0)
-        _, high_res = loader[0]
         
-        print(f"Low Resolution Image Shape: {low_res.shape}")
-        print(f"High Resolution Image Shape: {high_res.shape}")
-        
-        output_image = high_res.permute(1, 2, 0).cpu().numpy()  # (H, W, C)
-        output_image = (output_image * 255).astype(np.uint8)  # Rescale to [0,255]
-        # output_image = ImageConverter.convert_image(output_image, ImageColorConverter.YCrCb2BGR)  # Convert back to BGR
-
-        # Channels X Height X Width -> Height X Width X Channels
-        cv2.imshow("Low Resolution", low_res)
-        cv2.imshow("High Resolution", output_image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-        
-        return HttpResponse("Hello, world. You're at the polls index.")
+    return redirect("/")
 
 def test(request):
 
-
-    print("CUDA Available: ", torch.cuda.is_available())  # Should print True if GPU is detected
-    print("CUDA Device Count: ", torch.cuda.device_count())  # Number of GPUs available
-    print("Current CUDA Device: ", torch.cuda.current_device())  # Index of the active GPU
-    print("CUDA Device Name: ", torch.cuda.get_device_name(0))  # Name of the GPU
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return redirect("/")
 
