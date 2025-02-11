65f8677e156acfb696cacd4e6e3c6ac73463831a
LiocansG <nico6041@hotmail.be>
2025-02-11 20:23:41 +0100

    Making table a bit more stylish

--- derma_track_src/core/urls.py
+++ derma_track_src/core/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("patient", views.patient_profile, name = "patient"),
+    path("patient_profile", views.patient_profile, name = "patient_profile"),
     path("patient_list", views.patient_list, name = "patient_list"),
     
     path('visit_list', views.visit_list, name = 'visit_list'),
