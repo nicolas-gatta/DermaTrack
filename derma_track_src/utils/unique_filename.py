d05a98836f3ccc2716987a704e677996076f2159
LiocansG <nico6041@hotmail.be>
2025-03-14 21:53:08 +0100

    Help to find a unique name for files

---
+++ derma_track_src/utils/unique_filename.py
@@ -0,0 +1,30 @@
+import os
+
+def get_unique_filename(model_name, output_path):
+    """
+    Generates a unique filename by removing the extension and appending a numeric suffix if needed.
+
+    Args:
+        model_name (str): The original filename.
+        output_path (set): The patht to the folder we want to save our file.
+
+    Returns:
+        str: A unique filename without an extension.
+    """
+
+    base_name, ext = os.path.splitext(model_name)
+    
+    existing_names = {os.path.splitext(f)[0] for f in os.listdir(output_path)}
+    
+    if base_name in existing_names:
+        suffix = 1
+        new_name = f"{base_name}_{suffix}"
+        
+        while new_name in existing_names:
+            suffix += 1
+            new_name = f"{base_name}_{suffix}"
+        
+        base_name = new_name
+
+    return f"{base_name}.{ext}"
+    
