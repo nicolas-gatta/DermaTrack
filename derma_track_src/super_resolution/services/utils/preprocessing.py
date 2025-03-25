fbbd9486f39d2e2cdd174d0582c315711c7946ef
LiocansG <nico6041@hotmail.be>
2025-03-25 17:30:54 +0100

    Add progress indication for dataset creation in image preprocessing

--- derma_track_src/super_resolution/services/utils/preprocessing.py
+++ derma_track_src/super_resolution/services/utils/preprocessing.py
@@ -2,35 +2,35 @@ import cv2
 import os
 import numpy as np
 import h5py
-import re
+from tqdm import tqdm
 
 from super_resolution.services.utils.image_converter import ImageConverter, ImageColorConverter
 
 def _prepare_and_add_images(image_folder: str, scale: int, mode: ImageColorConverter, hi_res_images: h5py.Group, low_res_images: h5py.Group):
     
-    for count, file in enumerate(os.listdir(image_folder), start = 1):
-        if file.endswith(('.png', '.jpg', '.jpeg')):
+    images = [file for file in os.listdir(image_folder) if file.endswith(('.png', '.jpg', '.jpeg'))]
+    
+    for count, file in enumerate(tqdm(images, desc="Creating Dataset"), start = 1):
             
-            img_path = os.path.join(image_folder, file)
-            
-            hr = cv2.imread(img_path)  
+        img_path = os.path.join(image_folder, file)
+        
+        hr = cv2.imread(img_path)  
 
-            hr = ImageConverter.convert_image(hr, mode)
-            
-            lr = cv2.resize(hr, (hr.shape[1] // scale, hr.shape[0] // scale), interpolation = cv2.INTER_CUBIC)
-            
-            lr = cv2.resize(lr, (hr.shape[1], hr.shape[0]), interpolation = cv2.INTER_CUBIC)
-            
-            low_image = low_res_images.create_dataset(f"image_{count}", data = np.array(lr), dtype= np.uint8)
-            
-            hi_image = hi_res_images.create_dataset(f"image_{count}", data = np.array(hr), dtype= np.uint8)
-            
-            low_image.attrs["file"], hi_image.attrs["file"] = file, file
+        hr = ImageConverter.convert_image(hr, mode)
+        
+        lr = cv2.resize(hr, (hr.shape[1] // scale, hr.shape[0] // scale), interpolation = cv2.INTER_CUBIC)
+        
+        lr = cv2.resize(lr, (hr.shape[1], hr.shape[0]), interpolation = cv2.INTER_CUBIC)
+        
+        low_image = low_res_images.create_dataset(f"image_{count}", data = np.array(lr), dtype= np.uint8)
+        
+        hi_image = hi_res_images.create_dataset(f"image_{count}", data = np.array(hr), dtype= np.uint8)
+        
+        low_image.attrs["file"], hi_image.attrs["file"] = file, file
             
             
 def create_h5_image_file(input_path, scale, output_path, mode):
     
-    print(f"Creating H5 file for the {input_path} dataset")
     with h5py.File(output_path, 'w') as h5_file:
         
         hi_res_images = h5_file.create_group('hi_res')
@@ -38,5 +38,3 @@ def create_h5_image_file(input_path, scale, output_path, mode):
         low_res_images = h5_file.create_group('low_res')
         
         _prepare_and_add_images(input_path, scale, mode, hi_res_images, low_res_images)
-        
-    print(f"File created sucessfully at the path: {output_path}")
