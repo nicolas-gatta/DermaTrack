0e38443250d561ce98c7ebaf6d931f2ef4d8fcf4
LiocansG <nico6041@hotmail.be>
2025-04-01 18:24:10 +0200

    Fix import path for ResizeRule in json_manager.py

--- derma_track_src/super_resolution/services/utils/json_manager.py
+++ derma_track_src/super_resolution/services/utils/json_manager.py
@@ -3,7 +3,7 @@ import os
 
 from datetime import datetime
 from enum import Enum
-from derma_track_src.super_resolution.services.utils.prepare_dataset import ResizeRule
+from super_resolution.services.utils.prepare_dataset import ResizeRule
 
 from django.conf import settings
 
