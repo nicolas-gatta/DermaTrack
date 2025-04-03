b3ccef24d1f319abcee15a8f4c52a68c882434f5
LiocansG <nico6041@hotmail.be>
2025-04-03 20:48:18 +0200

    Refactor RunningAverage class to separate rounded average and improve count handling

--- derma_track_src/super_resolution/services/utils/running_average.py
+++ derma_track_src/super_resolution/services/utils/running_average.py
@@ -1,7 +1,7 @@
 class RunningAverage:
     def __init__(self):
         self.total = 0.0
-        self.count = 0
+        self.count = 0.0
         self.values = []
 
     def update(self, value):
@@ -20,13 +20,23 @@ class RunningAverage:
         self.count = 0.0
 
     @property
+    def rounded_average(self) -> float:
+        """
+        Returns the current average.
+        """
+        if self.count == 0:
+            return 0.0 
+        return round((self.total / self.count), 4)
+    
+    @property
     def average(self) -> float:
         """
         Returns the current average.
         """
-        if self.count == 0.0:
+        if self.count == 0:
             return 0.0 
-        return round((self.total / self.count), 4)
+        return self.total / self.count
+    
     
     @property
     def all_values(self) -> list:
