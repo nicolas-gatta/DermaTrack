4371d65da4a5487885f8e4a69a29d74c405cdb10
LiocansG <nico6041@hotmail.be>
2025-04-13 19:55:51 +0200

    Implement distance sensor button handling and serial data communication

--- distance_sensor_src/test_data_from_serial.py
+++ distance_sensor_src/test_data_from_serial.py
@@ -9,5 +9,13 @@ while True:
         try:
             data = json.loads(line)
             print("Received:", data)
+            
+            if(data["take_picture"]):
+                payload = {"picture_taken": True}
+                json_str = json.dumps(payload)
+                ser.write(json_str.encode('utf-8'))
+                print("Data Sended !")
+                break
+
         except json.JSONDecodeError:
             print("Received:", line)
