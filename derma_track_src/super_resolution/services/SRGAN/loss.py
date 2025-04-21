4bb0c5bf4185005d8775d275005507a1a9464693
LiocansG <nico6041@hotmail.be>
2025-04-21 14:39:49 +0200

    Refactor VGGLoss class: update VGG19 weights and feature extractor initialization; replace mse_loss with criterion for consistency

--- derma_track_src/super_resolution/services/SRGAN/loss.py
+++ derma_track_src/super_resolution/services/SRGAN/loss.py
@@ -5,12 +5,16 @@ from torchvision import models
 class VGGLoss(nn.Module):
     def __init__(self):
         super(VGGLoss, self).__init__()
-        vgg = models.vgg19(weights=models.VGG19_Weights.DEFAULT)
-        self.feature_extractor = nn.Sequential(*list(vgg.features.children())[:19])
-        for param in self.feature_extractor.parameters():
-            param.requires_grad = False
+        vgg = models.vgg19(weights=models.VGG19_Weights.IMAGENET1K_V1)
+        
+        self.feature_extractor = nn.Sequential(*list(vgg.features))
+        
+        for parameter in self.feature_extractor.parameters():
+            parameter.requires_grad = False
+   
+        self.criterion = nn.MSELoss()
 
     def forward(self, sr, hr):
         sr_features = self.feature_extractor(sr)
         hr_features = self.feature_extractor(hr)
-        return nn.functional.mse_loss(sr_features, hr_features.detach())
+        return self.criterion(sr_features, hr_features.detach())
