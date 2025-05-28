2642dc7735e3dc6c07cfa0f97b2109de9100f3a2
LiocansG <nico6041@hotmail.be>
2025-05-28 19:01:22 +0200

    Add functionality to handle missing fields in model state dictionaries and improve .pth file loading

---
+++ derma_track_src/utils/add_missing_data.py
@@ -0,0 +1,106 @@
+import torch
+from enum import Enum
+
+class ModelFile(str, Enum):
+    ARCHITECTURE = "architecture"
+    SCALE = "scale"
+    MODE = "color_mode"
+    INVERT_MODE = "invert_color_mode"
+    NEED_RESIZE = "need_resize"
+    PATCH_SIZE = "patch_size"
+    STRIDE = "stride"
+    MULTI = "multi_input"
+
+
+
+def add_missing_data(pth_path: str, values: dict):
+    try:
+        data = torch.load(pth_path, map_location='cpu', weights_only=True)
+
+        if isinstance(data, dict):
+            if 'model_state_dict' in data or 'state_dict' in data:
+                modified = False
+                for field in ModelFile:
+                    if field.value not in data:
+                        data[field.value] = values[field]
+                        print(f"Added missing field: {field.value} = {values[field]}")
+                        modified = True
+                    elif data[field.value] != values[field]:
+                        data[field.value] = values[field]
+                        print(f"Update field: {field.value} = {values[field]}")
+                        modified = True
+                    else:
+                        print(f"Field already have the right value: {field.value} = {data[field.value]}")
+
+                if modified:
+                    torch.save(data, pth_path)
+                    print(f"Updated .pth file saved: {pth_path}")
+                else:
+                    print("File is up to date.")
+            else:
+               print("The .pth file is not refactor to be use in the software") 
+
+    except Exception as e:
+        print("Error loading .pth file:", str(e))
+
+
+if __name__ == "__main__":
+    
+    basic_values = {
+        ModelFile.ARCHITECTURE: None,
+        ModelFile.SCALE: 2,
+        ModelFile.MODE: "BGR2RGB",
+        ModelFile.INVERT_MODE: "RGB2BGR",
+        ModelFile.NEED_RESIZE: False,
+        ModelFile.PATCH_SIZE: None,
+        ModelFile.STRIDE: None,
+        ModelFile.MULTI: False
+    }
+    
+    basic_values[ModelFile.ARCHITECTURE] = "SRCNN"
+    basic_values[ModelFile.SCALE] = 2
+    basic_values[ModelFile.PATCH_SIZE] = 32
+    basic_values[ModelFile.STRIDE] = 16
+    basic_values[ModelFile.NEED_RESIZE] = True
+    add_missing_data("C:\\Users\\Utilisateur\\Desktop\\Projet\\DermaTrack\\derma_track_src\\super_resolution\\models\\SRCNN_x2_BGR2YCrCb.pth", values = basic_values)
+    
+    # basic_values[ModelFile.ARCHITECTURE] = "SRCNN"
+    # basic_values[ModelFile.SCALE] = 4
+    # basic_values[ModelFile.PATCH_SIZE] = 32
+    # basic_values[ModelFile.STRIDE] = 16
+    # basic_values[ModelFile.NEED_RESIZE] = True
+    # add_missing_data("C:\\Users\\Utilisateur\\Desktop\\Projet\\DermaTrack\\derma_track_src\\super_resolution\\models\\SRCNN_x4_BGR2YCrCb.pth", values = basic_values)
+    
+    # basic_values[ModelFile.ARCHITECTURE] = "RRDBNet"
+    # basic_values[ModelFile.SCALE] = 4
+    # basic_values[ModelFile.PATCH_SIZE] = 128
+    # basic_values[ModelFile.NEED_RESIZE] = False
+    # add_missing_data("C:\\Users\\Utilisateur\\Desktop\\Projet\\DermaTrack\\derma_track_src\\super_resolution\\models\\Pretrained_RRDB_PSNR_x4_BGR2RGB.pth", values = basic_values)
+    
+    # basic_values[ModelFile.ARCHITECTURE] = "ESRGAN"
+    # basic_values[ModelFile.SCALE] = 4
+    # basic_values[ModelFile.PATCH_SIZE] = 128
+    # add_missing_data("C:\\Users\\Utilisateur\\Desktop\\Projet\\DermaTrack\\derma_track_src\\super_resolution\\models\\Pretrained_ESRGAN_x4_BGR2RGB.pth", values = basic_values)
+    
+    # basic_values[ModelFile.ARCHITECTURE] = "SRResNet"
+    # basic_values[ModelFile.SCALE] = 2
+    # basic_values[ModelFile.PATCH_SIZE] = 96
+    # add_missing_data("C:\\Users\\Utilisateur\\Desktop\\Projet\\DermaTrack\\derma_track_src\\super_resolution\\models\\Pretrained_SRResNet_x2_BGR2RGB.pth", values = basic_values)
+    
+    # basic_values[ModelFile.SCALE] = 4
+    # add_missing_data("C:\\Users\\Utilisateur\\Desktop\\Projet\\DermaTrack\\derma_track_src\\super_resolution\\models\\Pretrained_SRResNet_x4_BGR2RGB.pth", values = basic_values)
+    
+    # basic_values[ModelFile.ARCHITECTURE] = "SRGAN"
+    # basic_values[ModelFile.SCALE] = 2
+    # basic_values[ModelFile.PATCH_SIZE] = 96
+    # add_missing_data("C:\\Users\\Utilisateur\\Desktop\\Projet\\DermaTrack\\derma_track_src\\super_resolution\\models\\Pretrained_SRGAN_x2_BGR2RGB.pth", values = basic_values)
+    
+    # basic_values[ModelFile.SCALE] = 4
+    # add_missing_data("C:\\Users\\Utilisateur\\Desktop\\Projet\\DermaTrack\\derma_track_src\\super_resolution\\models\\Pretrained_SRGAN_x4_BGR2RGB.pth", values = basic_values)
+    
+    # basic_values[ModelFile.SCALE] = 4
+    # basic_values[ModelFile.PATCH_SIZE] = 256
+    # basic_values[ModelFile.MULTI] = True
+    # add_missing_data("C:\\Users\\Utilisateur\\Desktop\\Projet\\DermaTrack\\derma_track_src\\super_resolution\\models\\Pretrained_EDVR_x4_BGR2RGB.pth", values = basic_values)
+    
+    
--- derma_track_src/utils/print_info.py
+++ derma_track_src/utils/print_info.py
@@ -1,8 +1,8 @@
 import torch
 
-def print_pth_file_contents(pth_path):
+def print_pth_file_contents_dict_state(pth_path):
     try:
-        data = torch.load(pth_path, map_location=torch.device('cpu'))
+        data = torch.load(pth_path, map_location=torch.device('cpu'),  weights_only=True)
 
         if isinstance(data, dict):
             if 'model_state_dict' in data:
@@ -25,6 +25,26 @@ def print_pth_file_contents(pth_path):
     except Exception as e:
         print("Error loading .pth file:", str(e))
 
+def print_pth_file_contents(pth_path):
+    try:
+        data = torch.load(pth_path, map_location=torch.device('cpu'), weights_only=True)
+
+        if isinstance(data, dict):
+            if 'model_state_dict' in data or 'state_dict' in data:
+                for key, value in data.items():
+                    if key in ["model_state_dict", "state_dict"]:
+                        print(f"- {key}: Lot's of info here")
+                    else:
+                        print(f"- {key}: {value}")
+            
+            else:
+                print("The .pth file is not refactor to be use in the software")
+
+    except Exception as e:
+        print("Error loading .pth file:", str(e))
+        
 
 if __name__ == "__main__":
-    print_pth_file_contents("C:\\Users\\Utilisateur\\Downloads\\RRDB_ESRGAN_x4.pth")
+    # print_pth_file_contents_dict_state("C:\\Users\\Utilisateur\\Downloads\\RRDB_ESRGAN_x4.pth")
+    
+    print_pth_file_contents("C:\\Users\\Utilisateur\\Desktop\\Projet\\DermaTrack\\derma_track_src\\super_resolution\\models\\Pretrained_ESRGAN_x4.pth")
