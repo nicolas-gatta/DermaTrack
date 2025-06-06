a6d81859fc0eed88133c13694779c2957a82ee81
LiocansG <nico6041@hotmail.be>
2025-06-06 13:03:11 +0200

    Add docstring to L1_Charbonnier_loss class for better clarity

--- derma_track_src/super_resolution/services/EDVR/loss.py
+++ derma_track_src/super_resolution/services/EDVR/loss.py
@@ -2,6 +2,10 @@ import torch
 import torch.nn as nn
 
 class L1_Charbonnier_loss(nn.Module):
+    """
+    Class implementation of the L1 Charbonnier loss, a differentiable variant of the L1 loss that is less sensitive to outliers.
+    """
+    
     def __init__(self, epsilon = 1e-3):
         super(L1_Charbonnier_loss, self).__init__()
         self.epsilon = epsilon
